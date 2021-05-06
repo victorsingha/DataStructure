2a24738ec7f9b2d4827e914205403a7db7ecb78c
victor-singha <vicunite2@gmail.com>
2021-05-06 12:08:50 +0530

    [victor] Stack Code

---
+++ DataStructure/Stack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class Stack
+    {
+        private Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if(this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while(temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal void Peek()
+        {
+            if(this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of stack", this.top.data);
+        }
+        internal void Pop()
+        {
+            if(this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the deletec from stack", this.top.data);
+            this.top = this.top.next;
+        }
+        internal void IsEmpty()
+        {
+            while(this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
+    }
+}
